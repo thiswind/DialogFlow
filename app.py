62817da43486ac3b1d14b02bada2999f5f49f09a
thiswind <23764+thiswind@users.noreply.github.com>
2023-03-17 04:03:12 +0000

    UPDATE：实现了基本功能

--- app.py
+++ app.py
@@ -1,22 +1,42 @@
 from flask import Flask, render_template, request
+import openai
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def chat_page():
     return render_template('chat.html.jinja2')
 
+
 @app.route('/send_message', methods=['POST'])
 def send_message():
     user_message = request.form['message']
-    # 处理用户消息并生成回复
     response_message = process_user_message(user_message)
     return response_message
 
+
 def process_user_message(message):
-    # 在这里实现处理用户消息并生成回复的逻辑
-    response = "这里是一个示例回复，实际回复将由你的逻辑生成。"
-    return response
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                            messages=[{
+                                                "role":
+                                                "system",
+                                                "content":
+                                                "You are a helpful assistant."
+                                            }, {
+                                                "role": "user",
+                                                "content": message
+                                            }])
+    response_message = response['choices'][0]['message']['content']
+    return response_message
+
+
+@app.route('/set_api_key', methods=['POST'])
+def set_api_key():
+    api_key = request.form['api_key']
+    openai.api_key = api_key
+    return '', 204
+
 
 if __name__ == '__main__':
     app.run(debug=True)
